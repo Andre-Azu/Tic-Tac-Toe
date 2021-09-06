f5589aed2bfee9b7fc11d2ffa2fd2c438b32e154
andre azu <andreazuaa@gmail.com>
2021-09-06 16:21:10 +0300

    has an error

--- game.py
+++ game.py
@@ -11,12 +11,36 @@ v = ['','','','','','','','','']
 
 def board():
     print(" %s | %s | %s\n"
-          "____  ___  ____"
+          "____  ___  ___\n"
           " %s | %s | %s\n"
-          "____  ___  ____"
-          " %s | %s | %s\n"
-          "____  ___  ____")
+          "____  ___  ___\n"
+          " %s | %s | %s\n" % v[0],v[1],v[2],v[3],v[4],v[5],v[6],v[7],v[8])
 
 
 
-instruction()
+def userinput(char):
+    u = int(input("were do you play? %s"%(char)))
+    if u<=0 or u>=9:
+        print("invalid number")
+        return False
+    if v[u-1] =='x' or v[u-1]== '0':
+        print("you already have a charachter")
+    return u
+
+
+def main():
+    for i in range(9):
+        if i %2 == 0:
+            character='x'
+        else:
+            character = 'o' 
+        instruction()
+        board() 
+        user = userinput(character)
+        while user == False:
+            user = userinput(character)
+        user= user-1
+        v[user] =character  
+main()
+
+
