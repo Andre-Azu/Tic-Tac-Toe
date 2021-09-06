6972dffde51de9b82e921496f7b7681bfab7fd86
andre azu <andreazuaa@gmail.com>
2021-09-06 14:08:54 +0300

    add files

---
+++ game.py
@@ -0,0 +1,22 @@
+def instruction():
+    print("These are the places where you place your character")
+    print("1 | 2 | 3\n"
+          "___ ___ __\n"
+          "4 | 5 | 6\n"
+          "___ ___ __\n"
+          "7 | 8 | 9\n"
+          "___ ___ __\n")
+
+v = ['','','','','','','','','']
+
+def board():
+    print(" %s | %s | %s\n"
+          "____  ___  ____"
+          " %s | %s | %s\n"
+          "____  ___  ____"
+          " %s | %s | %s\n"
+          "____  ___  ____")
+
+
+
+instruction()
